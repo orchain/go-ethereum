cce4ca1d3622925d50455a28a9374467afb1ee07
yemille <yemille@gmail.com>
2025-05-17 17:57:00 +0800

    Starting Geth on new gas price

--- eth/gasprice/gasprice.go
+++ eth/gasprice/gasprice.go
@@ -206,7 +206,11 @@ func (oracle *Oracle) SuggestTipCap(ctx context.Context) (*big.Int, error) {
 		}
 		results = append(results, res.values...)
 	}
+
 	price := lastPrice
+	if head.Number.Cmp(big.NewInt(params.NewBaseFeeBlockHeight)) > 0 {
+		price = big.NewInt(params.NewMinBaseFee)
+	}
 	if len(results) > 0 {
 		slices.SortFunc(results, func(a, b *big.Int) int { return a.Cmp(b) })
 		price = results[(len(results)-1)*oracle.percentile/100]

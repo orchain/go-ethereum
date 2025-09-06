70e234156cd4a34ef7736bd8be2f998960c49adf
yemille <yemille@gmail.com>
2025-09-06 16:43:12 +0800

    new api

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -19,7 +19,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/ethereum/go-ethereum/params"
 	"os"
 	"sort"
 	"strconv"
@@ -273,7 +272,7 @@ func main() {
 // This function should be called before launching devp2p stack.
 func prepare(ctx *cli.Context) {
 	// If we're running a known preset, log it for convenience.
-	log.Info(fmt.Sprintf("Starting Geth on new gas price, new gas %v, block height %v", params.NewMinBaseFee, params.NewBaseFeeBlockHeight))
+	log.Info(fmt.Sprintf("Starting Geth on new node"))
 	switch {
 	case ctx.IsSet(utils.GoerliFlag.Name):
 		log.Info("Starting Geth on Görli testnet...")
